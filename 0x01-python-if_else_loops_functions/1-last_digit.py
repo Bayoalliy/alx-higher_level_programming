144abf11e7f9a4c019d32680ccf8e34782239882
Bayoalliy <adebayoalliy@gmail.com>
2022-11-29 03:56:30 -0800

    negative modulus

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = ((number)**2)**0.5 % 10
-string = f"Last digit of {number} is {last_digit:.0f}"
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+string = f"Last digit of {number} is {last_digit}"
 if last_digit > 5:
     print(f"{string} and is greater than 5")
 elif last_digit == 0:
