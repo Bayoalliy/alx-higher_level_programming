a81a8dddba1aa773b5b6961761ae5fa7fb0cc67d
bayoalliy <adebayoalliy@gmail.com>
2023-10-31 06:03:15 -0700

    low memory

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,10 +2,11 @@
 
 """
 
-This is a class that prevents user from 
+This is a class that prevents user from
 dynamically creating new instances attributes.
 
 """
+
 
 class LockedClass:
     __slots__ = ["first_name"]
