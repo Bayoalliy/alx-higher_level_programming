ceb9d9095cbaea7ae00ae688daf0ab73f34673f5
bayo <adebayoalliy@gmail.com>
2023-11-13 12:40:35 -0800

    documetation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from models.base import Base
 
 """
 This module contains a Rectangle class that is a child of Base.
 
 """
+from models.base import Base
 
 
 class Rectangle(Base):
