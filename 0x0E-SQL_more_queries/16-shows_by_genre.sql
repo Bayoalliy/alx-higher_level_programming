f0c0cb84a4ab8aa5b8b97a70b0a6729a838e8deb
adebayo <adebayoalliy@gmail.com>
2024-05-23 17:29:25 -0700

    join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
 RIGHT JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title, tv_genres.name ASC;
