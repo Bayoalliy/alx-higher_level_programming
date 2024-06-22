0408b2e3971d1fa3e22cfd90bb8e0f786debab85
adebayo <adebayoalliy@gmail.com>
2024-06-22 14:02:04 -0700

    try

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     session = Session()
 
     # Querying data
-    states = session.query(State).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
