897f72825d7c09e055a1f74e62a9a993a863561b
adebayo <adebayoalliy@gmail.com>
2024-06-22 01:45:55 -0700

    list comp

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,11 +18,5 @@ if __name__ == '__main__':
     ON states.id = cities.state_id WHERE states.name = %s"
     cur.execute(query, (args[4],))
     states = cur.fetchall()
-    i = 0
-    while (i < len(states)):
-        for elem in states[i]:
-            print(elem, end="")
-            if i != (len(states) - 1):
-                print(", ", end="")
-        i += 1
-    print("")
+
+    print(", ".join([state[0] for state in states]))
