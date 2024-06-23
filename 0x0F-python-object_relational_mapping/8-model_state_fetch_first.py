02f68e6c2669eb62baf3164cf45e202be32cd221
adebayo <adebayoalliy@gmail.com>
2024-06-22 17:09:50 -0700

    now

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,8 +5,8 @@ lists all State objects from the database hbtn_0e_6_usa
 
 import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
