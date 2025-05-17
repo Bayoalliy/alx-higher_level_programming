78979286ce1851bbf0921814f5f02902e7da1b02
adebayo <adebayoalliy@gmail.com>
2025-05-17 01:07:52 -0700

    post

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-content=$(<$2); curl -sX POST $content $1
+curl -s -X POST $2 $1
