08d0287a26e4d3970751953f826d7847b97b361a
bayo <adebayoalliy@gmail.com>
2025-05-18 10:40:26 -0700

    req

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     res = requests.post(url, data=data)
     try:
         res = res.json()
-    except ValueError:
+    except:
         print("Not a valid JSON")
 
     if res:
