31212fe4cdd186079e60781b5bbe8bb16fb5f9a6
adebayoalliy <adebayoalliy@gmail.com>
2025-06-18 08:14:49 -0700

    loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+    if (error) {
+      console.log(error);
+    }
+  })
+});
