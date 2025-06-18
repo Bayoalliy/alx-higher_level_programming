df9e25ce0ba5350a9bc3441e6bd04a206c41f8e2
adebayoalliy <adebayoalliy@gmail.com>
2025-06-18 07:49:08 -0700

    hh

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
+let nFilms = 0;
 
-request(process.argv[2], (err, res, body) => {
-  let count = 0;
-  const wedge = 'https://swapi-api.alx-tools.com/api/people/18/';
-  if (err) {
-    console.log(err);
-    return;
-  }
-  for (const film of JSON.parse(body).results) {
-    for (const actorsUrl of film.characters) {
-      if (actorsUrl === wedge) {
-        count += 1;
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const results = resp.results;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
+          nFilms++;
+        }
       }
     }
   }
-  console.log(count);
+  console.log(nFilms);
 });
